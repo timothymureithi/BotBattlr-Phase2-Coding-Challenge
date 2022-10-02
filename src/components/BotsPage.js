a6fb46021eac811a9f746661193c40624d22ba87
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-10-02 10:59:57 +0300

    Passed down props

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,4 +1,4 @@
-import React, {useState, useState} from "react";
+import React, {useState, useEffect} from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
@@ -10,18 +10,55 @@ function BotsPage() {
   const [bots, setBots] = useState([]);
   const [botArmy, setBotsArmy] = useState([]);
 
-  //fetch request
-  useEffect(() =>{
+  //fetch request to render profiles
+  useEffect(() => {
     fetch("http://localhost:8002/bots")
     .then(response => response.json())
     .then(bots => setBots(bots))
   }, []);
 
+  //Create bot army
+  function addBotsList(botData){
+    if(!botArmy.find(bot => bot === botData)){
+      const findBot = bots.find(bot => bot === botData)
+      setBotsArmy([...botArmy, findBot])
+    } 
+  }
+
+  //Delete bot
+  function deletePermanently (botData){
+  if(botArmy.find((bot) => bot === botData)){
+    const filterBots = bots.filter(bot => bot !== botData)
+    const filterBotArmy = botData.filter(bot => bot !== botData)
+
+    setBots(filterBots)
+    setBotsArmy(filterBotArmy)
+
+    fetch(`http://localhost:8002/bots/${botData.id}`, {
+      method: "DELETE"
+    })
+  }
+}
+
+  function dischargeBot(botData) {
+    const botList = botArmy.filter((bot)=> bot !== botData)
+    setBotsArmy(botList)
+  }
+
+//botArmy, removeBot, dischargeBot <YourBotArmy/>
+//bots, addBot, dischargeBot <BotCollection/>
 
   return (
     <div>
-      <YourBotArmy />
-      <BotCollection />
+      <YourBotArmy
+       botArmy={botArmy} 
+       removeBot={dischargeBot}
+       dischargeBot={deletePermanently} />
+
+      <BotCollection 
+      bots={bots}
+      addBot={addBotsList}
+      dischargeBot={deletePermanently} />
     </div>
   )
 }
