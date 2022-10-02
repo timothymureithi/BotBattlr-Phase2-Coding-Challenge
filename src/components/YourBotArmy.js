31fa853351c3eb50c953488d090e1b600cce2fd2
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-10-02 12:38:31 +0300

    Added BotCard prop

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,18 +1,23 @@
 import React from "react";
 import BotCard from "./BotCard";
 
-function YourBotArmy({dischargeBot, bots, removeBot}) {
-/**Discharge a bot from their service forever, by clicking the red button marked
-  "x" */
+function YourBotArmy ({botArmy, removeBot, dischargeBot}) {
+  //your bot army code here...
+
+
+
+//BotCard -> bot, botCard, handleDischargeBot 
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {bots.map((bot) => (
-            <BotCard bot={bot} 
-            handleBot={removeBot}
-            handleDeleteBot={dischargeBot}/>
+          {botArmy.map((bot) => (
+          <BotCard 
+          bot={bot} 
+          botCard={removeBot}
+          dischargeBot={dischargeBot}/>
           ))}
+          Your Bot Army
         </div>
       </div>
     </div>
